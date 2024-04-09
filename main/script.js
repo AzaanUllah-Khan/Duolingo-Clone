90cfde904a77d60ff58328a57f14d4032c669f7c
AzaanUllah-Khan <azaanullahkhan291@gmail.com>
2024-04-09 06:48:13 +0500

    loader added and made functional

--- main/script.js
+++ main/script.js
@@ -1,6 +1,12 @@
 function switchActive(id){
+    document.getElementById("main").classList.add("animate")
     var currentActive = document.getElementsByClassName("active")
     currentActive[0]?.classList.remove("active")
     document.getElementById(id).classList.add("active")
-    document.getElementById("iframe").src = `./iframes/${id}.html`
+    setTimeout(() => {    
+        document.getElementById("iframe").src = `./iframes/${id}.html`
+    }, 1000);
+    setTimeout(()=>{
+        document.getElementById("main").classList.remove("animate")
+    },1000)
 }
