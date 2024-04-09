7ca3e9c8bedc725fa4752649794c44f56e86054a
AzaanUllah-Khan <azaanullahkhan291@gmail.com>
2024-04-09 07:22:50 +0500

    sentenses updated in each pafe inside iframes

--- main/script.js
+++ main/script.js
@@ -1,12 +1,12 @@
-function switchActive(id){
-    document.getElementById("main").classList.add("animate")
+function switchActive(id) {
+    document.getElementById("main")?.classList.add("animate")
     var currentActive = document.getElementsByClassName("active")
     currentActive[0]?.classList.remove("active")
-    document.getElementById(id).classList.add("active")
-    setTimeout(() => {    
+    document.getElementById(id)?.classList.add("active")
+    setTimeout(() => {
         document.getElementById("iframe").src = `./iframes/${id}.html`
     }, 1000);
-    setTimeout(()=>{
-        document.getElementById("main").classList.remove("animate")
-    },1000)
+    setTimeout(() => {
+        document.getElementById("main")?.classList.remove("animate")
+    }, 1000)
 }
