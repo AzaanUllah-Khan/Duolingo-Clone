d2245607eef5eaf9f99adec436dbb5ba980f7728
AzaanUllah-Khan <azaanullahkhan291@gmail.com>
2024-04-12 00:41:57 +0500

    image uploading through profile page

--- main/iframes/profile.js
+++ main/iframes/profile.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-app.js";
-import { getAuth,onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-auth.js";
-import { getFirestore,doc, getDoc } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-firestore.js";
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-auth.js";
+import { getFirestore, doc, getDoc } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-firestore.js";
+import { getStorage, ref, uploadBytes } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-storage.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyCIrlFqcZrbpluUnBZ1bL6_8SsiwBmISi4",
@@ -15,14 +16,17 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
-onAuthStateChanged(auth, async(user) => {
+const storage = getStorage();
+var email;
+
+onAuthStateChanged(auth, async (user) => {
     if (user) {
         const docRef = doc(db, "Users", user.uid);
         const docSnap = await getDoc(docRef);
-
+        email = user.email
         if (docSnap.exists()) {
             document.getElementById("name").innerHTML = docSnap.data().name
-            document.getElementById("nick").innerHTML = (docSnap.data().name).split(" ")[0]+"1"
+            document.getElementById("nick").innerHTML = (docSnap.data().name).split(" ")[0] + "1"
             document.getElementById("join").innerHTML = `Joined ${docSnap.data().month} ${docSnap.data().year}`
         } else {
             console.log("No such document!");
@@ -36,14 +40,23 @@ document.getElementById("svg").viewBox.baseVal.y = y
 document.getElementById("svg").viewBox.baseVal.width = "82"
 document.getElementById("svg").viewBox.baseVal.height = "66"
 
-document.getElementById("f").addEventListener("click",()=>{
+document.getElementById("f").addEventListener("click", () => {
     document.getElementById("f").classList.add("active")
     document.getElementById("fi").classList.remove("active")
     document.getElementById("ma").innerHTML = `<img src="https://d35aaqx5ub95lt.cloudfront.net/images/profile/a925a18c6be921a81bf0e13102983168.svg" alt="">
     <p>Learning is more fun and effective when you connect with others.</p>`
 })
-document.getElementById("fi").addEventListener("click",()=>{
+document.getElementById("fi").addEventListener("click", () => {
     document.getElementById("fi").classList.add("active")
     document.getElementById("f").classList.remove("active")
     document.getElementById("ma").innerHTML = `<h1>No Followers Yet</h1>`
 })
+function uploadPics() {
+    const storageRef = ref(storage, email);
+    const file = document.getElementById("file").files[0]
+
+    uploadBytes(storageRef, file).then(() => {
+        console.log('Uploaded a blob or file!');
+    });
+}
+document.getElementById("file").addEventListener("change",uploadPics)
