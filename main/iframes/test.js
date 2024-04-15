9aeb4c1998744f8bb96148622d4a08c857b69dee
AzaanUllah-Khan <azaanullahkhan291@gmail.com>
2024-04-16 01:34:22 +0500

    test.js created option active handled

---
+++ main/iframes/test.js
@@ -0,0 +1,11 @@
+var option = document.getElementsByTagName("p")
+for (i = 0; i < option.length; i++) {
+    option[i].addEventListener("click", (event) => { buttonClick(event.target) })
+}
+function buttonClick(btn) {
+    for (i = 0; i < option.length; i++) {
+        option[i].classList.remove("active")
+    }
+    var btnTxt = btn.innerHTML
+    btn.classList.add("active")
+}
