81745b32bed13dd3601d2faab48c71fbabb4e760
AzaanUllah-Khan <azaanullahkhan291@gmail.com>
2024-04-16 03:01:18 +0500

    test section seperated and option checking mechanism added

---
+++ main/test.js
@@ -0,0 +1,87 @@
+document.getElementById("lang").innerHTML = localStorage.getItem("language")
+localStorage.setItem("lifelines", 5)
+var q = 0
+var questionsAnswers = [
+    {
+        question: "Monday",
+        option1: "Friday",
+        option2: "Monday",
+        option3: "Tuesday",
+        option4: "thursday",
+        correctOpt: "2"
+    }, {
+        question: "How are you ?",
+        option1: "Hello",
+        option2: "What time is it ?",
+        option3: "How are you ?",
+        option4: "What is your name ?",
+        correctOpt: "3"
+    }, {
+        question: "Father",
+        option1: "Mother",
+        option2: "Uncle",
+        option3: "Son",
+        option4: "Father",
+        correctOpt: "4"
+    }, {
+        question: "Good Night",
+        option1: "Good Morning",
+        option2: "I am fine",
+        option3: "Hello",
+        option4: "Good Night",
+        correctOpt: "4"
+    }, {
+        question: "I am a Boy",
+        option1: "I am a Boy",
+        option2: "I am a Girl",
+        option3: "My name is",
+        option4: "What is your name ?",
+        correctOpt: "1"
+    }
+]
+var option = document.getElementsByTagName("p")
+function setQ() {
+    document.getElementById("ll").innerHTML = localStorage.getItem("lifelines");
+    document.getElementById("checkBtn").disabled = true;
+    document.getElementById("question").innerHTML = questionsAnswers[q].question;
+    for (let i = 0; i < option.length; i++) {
+        option[i].classList.remove("active");
+        option[i].id = i+1
+        let apiUrl = `https://api.mymemory.translated.net/get?q=${questionsAnswers[q][`option${i + 1}`]}&langpair=en|${localStorage.getItem("language")}`;
+        fetch(apiUrl)
+            .then(res => res.json())
+            .then(data => {
+                option[i].innerHTML = data.responseData.translatedText
+            });
+    }
+}
+
+for (i = 0; i < option.length; i++) {
+    option[i].addEventListener("click", (event) => { buttonClick(event.target) })
+}
+function buttonClick(btn) {
+    for (i = 0; i < option.length; i++) {
+        option[i].classList.remove("active")
+    }
+    btn.classList.add("active")
+    document.getElementById("checkBtn").disabled = false
+}
+function increaseQ() {
+    var btn = document.getElementsByClassName("active")[0]
+    if(btn.id == questionsAnswers[q].correctOpt){
+        alert("A")
+    }
+    q++
+    setQ()
+}
+document.getElementById("checkBtn").addEventListener("click", increaseQ)
+document.getElementById("skip").addEventListener("click", skip)
+function skip() {
+    ll = Number(localStorage.getItem("lifelines"))
+    if (ll >= 2) {
+        q++
+        localStorage.setItem("lifelines", ll - 1)
+        setQ()
+    }
+}
+setQ()
