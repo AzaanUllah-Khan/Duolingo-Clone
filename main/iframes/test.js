8d6fcd6f0eebb91ef3d8cf7c248e87c79f0a7f16
AzaanUllah-Khan <azaanullahkhan291@gmail.com>
2024-04-16 02:29:19 +0500

    showing options in the language which user in learning

--- main/iframes/test.js
+++ main/iframes/test.js
@@ -1,55 +1,60 @@
 document.getElementById("lang").innerHTML = localStorage.getItem("language")
-localStorage.setItem("lifelines",5)
+localStorage.setItem("lifelines", 5)
 var q = 0
 var questionsAnswers = [
     {
-        question:"Monday",
-        option1:"Friday",
-        option2:"Monday",
-        option3:"Tuesday",
-        option4:"Thursday",
-        correctOpt:"2"
-    },{
-        question:"How are you ?",
-        option1:"Hello",
-        option2:"What time is it ?",
-        option3:"How are you ?",
-        option4:"What is your name ?",
-        correctOpt:"3"
-    },{
-        question:"Father",
-        option1:"Mother",
-        option2:"Uncle",
-        option3:"Son",
-        option4:"Father",
-        correctOpt:"4"
-    },{
-        question:"Good Night",
-        option1:"Good Morning",
-        option2:"I am fine",
-        option3:"Hello",
-        option4:"Good Night",
-        correctOpt:""
-    },{
-        question:"I am a Boy",
-        option1:"I am a Boy",
-        option2:"I am a Girl",
-        option3:"My name is",
-        option4:"What is your name ?",
-        correctOpt:"1"
+        question: "Monday",
+        option1: "Friday",
+        option2: "Monday",
+        option3: "Tuesday",
+        option4: "Thursday",
+        correctOpt: "2"
+    }, {
+        question: "How are you ?",
+        option1: "Hello",
+        option2: "What time is it ?",
+        option3: "How are you ?",
+        option4: "What is your name ?",
+        correctOpt: "3"
+    }, {
+        question: "Father",
+        option1: "Mother",
+        option2: "Uncle",
+        option3: "Son",
+        option4: "Father",
+        correctOpt: "4"
+    }, {
+        question: "Good Night",
+        option1: "Good Morning",
+        option2: "I am fine",
+        option3: "Hello",
+        option4: "Good Night",
+        correctOpt: ""
+    }, {
+        question: "I am a Boy",
+        option1: "I am a Boy",
+        option2: "I am a Girl",
+        option3: "My name is",
+        option4: "What is your name ?",
+        correctOpt: "1"
     }
 ]
+var option = document.getElementsByTagName("p")
 function setQ() {
     document.getElementById("ll").innerHTML = localStorage.getItem("lifelines");
     document.getElementById("checkBtn").disabled = true;
     document.getElementById("question").innerHTML = questionsAnswers[q].question;
-    for (i = 0; i < option.length; i++) {
+    for (let i = 0; i < option.length; i++) {
         option[i].classList.remove("active");
-        option[i].innerHTML = questionsAnswers[q][`option${i+1}`];
+        let apiUrl = `https://api.mymemory.translated.net/get?q=${questionsAnswers[q][`option${i + 1}`]}&langpair=en|${localStorage.getItem("language")}`;
+        fetch(apiUrl)
+            .then(res => res.json())
+            .then(data => {
+                option[i].innerHTML = data.responseData.translatedText
+            });
     }
 }
 
-var option = document.getElementsByTagName("p")
 for (i = 0; i < option.length; i++) {
     option[i].addEventListener("click", (event) => { buttonClick(event.target) })
 }
@@ -60,17 +65,17 @@ function buttonClick(btn) {
     btn.classList.add("active")
     document.getElementById("checkBtn").disabled = false
 }
-document.getElementById("checkBtn").addEventListener("click",increaseQ)
-document.getElementById("skip").addEventListener("click",skip)
-function increaseQ(){
+document.getElementById("checkBtn").addEventListener("click", increaseQ)
+document.getElementById("skip").addEventListener("click", skip)
+function increaseQ() {
     q++
     setQ()
 }
-function skip(){
+function skip() {
     ll = Number(localStorage.getItem("lifelines"))
-    if(ll>=2){
+    if (ll >= 2) {
         q++
-        localStorage.setItem("lifelines",ll-1)
+        localStorage.setItem("lifelines", ll - 1)
         setQ()
     }
 }
