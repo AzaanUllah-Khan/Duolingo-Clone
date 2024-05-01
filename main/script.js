9235eee554f94c82cb1a69844ae76f3cc78b7c20
AzaanUllah-Khan <azaanullahkhan291@gmail.com>
2024-05-01 13:15:41 +0500

    iframe > language translator made responsive

--- main/script.js
+++ main/script.js
@@ -1,14 +1,17 @@
 function switchActive(id) {
-    document.getElementById('main').style.zIndex = '1'
-    document.getElementById('aside').style.zIndex = '0'
-    document.getElementById("main")?.classList.add("animate")
-    var currentActive = document.getElementsByClassName("active")
-    currentActive[0]?.classList.remove("active")
-    document.getElementById(id)?.classList.add("active")
+    var screenWidth = window.innerWidth;
+    if (screenWidth <= 800) {
+        document.getElementById('main').style.zIndex = '1';
+        document.getElementById('aside').style.zIndex = '0';
+    }
+    var currentActive = document.getElementsByClassName("active");
+    currentActive[0]?.classList.remove("active");
+    document.getElementById(id)?.classList.add("active");
     setTimeout(() => {
-        document.getElementById("iframe").src = `./iframes/${id}.html`
+        document.getElementById("iframe").src = `./iframes/${id}.html`;
+        document.getElementById("main")?.classList.add("animate");
     }, 1000);
     setTimeout(() => {
-        document.getElementById("main")?.classList.remove("animate")
-    }, 1000)
+        document.getElementById("main")?.classList.remove("animate");
+    }, 1000);
 }
